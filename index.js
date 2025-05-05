d6e5f8ec4109e8902d382c0cacf14e75d30cb65f
zaidkhan-div <zaidscestudent@gmail.com>
2025-05-05 20:27:24 +0500

    .

--- index.js
+++ index.js
@@ -1,17 +1,77 @@
+// import express from 'express';
+// import nodemailer from 'nodemailer';
+// import cors from 'cors';
+// import fs from 'fs'
+
+// const app = express();
+// const port = 3000;
+// app.use(express.json());
+// app.use(cors({
+//     allowedHeaders: '*',
+//     methods: ['POST',],
+//     origin: '*',
+//     preflightContinue: true
+// }));
+
+
+
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: "zaidscestudent@gmail.com",
+//         pass: "atdy bxeo zlci pcna",
+//     }
+// })
+
+// app.post('/sendmail', async (req, res) => {
+//     const { company, user_name, phone, email, message, customer_type } = req.body;
+
+//     fs.readFile('Form.html', 'utf-8', (err, data) => {
+//         if (err) {
+//             return res.status(500).send('Error reading template');
+//         }
+
+//         const mailOptions = {
+//             from: email,
+//             to: 'salmankarim.khan42@gmail.com',
+//             subject: 'New Contact Form Submission',
+//             html: data.replace('{{company}}', company)
+//                 .replace('{{user_name}}', user_name)
+//                 .replace('{{phone}}', phone)
+//                 .replace('{{email}}', email)
+//                 .replace('{{message}}', message)
+//                 .replace('{{customer_type}}', customer_type),
+//         };
+
+//         transporter.sendMail(mailOptions, (error, info) => {
+//             if (error) {
+//                 return res.status(500).send('Error sending email');
+//             }
+//             res.status(200).send('Email sent successfully');
+//         });
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//     res.send('This Checking')
+// })
+
+// app.listen(port, () => {
+//     console.log('Server is Running on port ', port);
+// })
+
 import express from 'express';
 import nodemailer from 'nodemailer';
 import cors from 'cors';
-import fs from 'fs'
+import fs from 'fs';
 
 const app = express();
-const port = 3000;
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['POST'],
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: '*',
 }));
-
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -19,9 +79,9 @@ const transporter = nodemailer.createTransport({
         user: "zaidscestudent@gmail.com",
         pass: "atdy bxeo zlci pcna",
     }
-})
+});
 
-app.post('/sendmail', async (req, res) => {
+app.post('/sendmail', (req, res) => {
     const { company, user_name, phone, email, message, customer_type } = req.body;
 
     fs.readFile('Form.html', 'utf-8', (err, data) => {
@@ -51,9 +111,9 @@ app.post('/sendmail', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('This Checking')
-})
+    res.send('This is working');
+});
 
-app.listen(port, () => {
-    console.log('Server is Running on port ', port);
-})
+// ⛔️ Remove app.listen()
+// ✅ Export the handler for Vercel
+export default app;
