aa0877ef4c900499e2e9a98d3d90faee5964d557
zaidkhan-div <zaidscestudent@gmail.com>
2025-05-08 17:20:37 +0500

    make new next project

--- src/Components/Container.jsx
+++
@@ -1,8 +0,0 @@
-const Container = ({children}) => {
-    return (
-        <div className="w-full max-w-[1272px] mx-auto px-4">
-            {children}
-        </div>
-    )
-}
-export default Container;
